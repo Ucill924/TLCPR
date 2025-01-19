09b4e9a561588cc6c21f03b7a9f3335ddd39792b
Ucillny <72456107+Ucill924@users.noreply.github.com>
2025-01-20 02:39:48 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+{
+  "gwei_input": 0.2,
+  "rpc_url": "https://testnet-rpc3.cypher.z1labs.ai",
+  "chain_id": 10112,
+  "min_sleep": 5,
+  "max_sleep": 10,
+  "edai": "0xb7229F1209d4c5bdc47996da3C64BecD84084025",
+  "max_txs": 10
+}
